b46859fc255c91b96114d60fc43ba3ee83a394bc
Rajnandini <rajnandinipatra2003@gmail.com>
2024-11-30 01:56:17 +0530

    HTML Basics

---
+++ HTML Projects/@PracticeProject/Technical Documantation Project/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int bobo = 1;
+cout<<" enter bobo name";
+cin>>bobo;
+
+
+while (bobo>1000)
+{
+    cout<<" Bobo I love u";
+}
+
+
+
+
+
+
+    return 0;
+}
